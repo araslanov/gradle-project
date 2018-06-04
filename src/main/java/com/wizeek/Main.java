3b66a56980580000b11527105331f7c283271f4a
Artur <wizeek80@gmail.com>
2018-06-04 23:39:58 +0300

    added main method

---
+++ src/main/java/com/wizeek/Main.java
@@ -0,0 +1,10 @@
+package com.wizeek;
+
+/**
+ * Created by Artur on 6/4/2018.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
